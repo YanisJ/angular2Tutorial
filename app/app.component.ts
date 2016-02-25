2f66167796da1e85e48c220696046dccf0af74e2
yanisj <553124029@qq.com>
2016-02-25 15:10:42 +0800

    first component

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'my-app',  // this is a simple CSS selector
+  template: '<h1>My First Angular 2 App</h1>'
+})
+
+export class AppComponent { }  // "export" turns the file into a module.
